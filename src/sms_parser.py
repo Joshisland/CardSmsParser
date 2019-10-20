847bbb3da3f4893e737d2055ef2d83369cec0c32
Josh <shinhaeng.jo@nielsen.com>
2019-10-20 23:42:09 +0900

    Introduced ParsedResult class

--- src/sms_parser.py
+++ src/sms_parser.py
@@ -1,6 +1,71 @@
 #!/usr/bin/python
 # coding: utf-8
 import re
+from collections import namedtuple
+
+
+class ParsingFailedError(Exception):
+    pass
+
+
+class ParsedResultBase(object):
+    """Default values"""
+
+    def __init__(self):
+        self.installment = '일시불'
+        self.hour = 3
+        self.minute = 30
+        self.is_cancelled = False
+
+
+class ParsedResult(ParsedResultBase):
+    def __init__(self, data):
+        super(ParsedResult, self).__init__()
+        for key, value in data.items():
+            setattr(self, key, self._wrap(value))
+
+    def __str__(self):
+        return ",".join([str(data) for data in self.__dict__.values()])
+
+    def to_str(self):
+        # 2019. 6. 2 오후 11:03:00
+        ampm = "오전" if self.hour < 12 else "오후"
+        date = "2019. %s. %s %s %s:%s:00" % (self.month, self.day, ampm, self.hour % 12, self.minute)
+        datalist = [date, self.place, "", "", date, self.amount, "카드구매", self.card_name]
+        if self.is_cancelled:
+            datalist.append("CANCEL")
+        return ",".join(map(str, datalist))
+
+    def _wrap_value(self, value):
+        """Convert comma separated number in string into int or float"""
+        if isinstance(value, str):
+            temp = value.replace(',', '')
+            try:
+                return int(temp) if int(temp) == float(temp) else float(temp)
+            except ValueError:
+                pass
+        return value
+
+    def _wrap(self, value):
+        """Recursively convert data structure"""
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return ParsedResult(value) if isinstance(value, dict) else self._wrap_value(value)
+
+
+class Struct(object):
+    """Comment removed"""
+
+    def __init__(self, data):
+        for name, value in data.iteritems():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return Struct(value) if isinstance(value, dict) else value
 
 
 class SmsParserBase(object):
@@ -9,9 +74,13 @@ class SmsParserBase(object):
 
     def parse(self, target, debug=False):
         if debug:
-            print("target:", target)
+            print("target:%s" % target)
 
         res = self._parse_internal(target)
+        try:
+            return ParsedResult(res['group'])
+        except TypeError as err:
+            raise ParsingFailedError()
 
         if debug:
             for key in ['pattern', 'result', 'group']:
@@ -53,7 +122,21 @@ class HyundaiCardParser(SmsParserBase):
                          r'(?P<place>.*)\n'
                          r'누적(?P<accumulated>[\d,]*)원')
         res = pat.search(target)
-        return {'result': res, 'pattern': pat, 'group': res.groupdict()}
+        if res:
+            return {'result': res, 'pattern': pat, 'group': res.groupdict()}
+
+        cancel_pat = re.compile(r'\[Web발신\]\n'
+                                r'(?P<card_name>.*) 취소\n'
+                                r'(?P<name_part>.*)\n'
+                                r'(?P<amount>[\d,]*)원 (?P<installment>.*)\n'
+                                r'(?P<month>\d*)/(?P<day>\d*) (?P<hour>\d*):(?P<minute>\d*)\n'
+                                r'(?P<place>.*)\n'
+                                r'누적(?P<accumulated>[\d,]*)원')
+        res = cancel_pat.search(target)
+        if res:
+            groupdict = res.groupdict()
+            groupdict['is_cancelled'] = True
+            return {'result': res, 'pattern': cancel_pat, 'group': groupdict}
 
 
 class WooriCardParser(SmsParserBase):
@@ -69,4 +152,15 @@ class WooriCardParser(SmsParserBase):
                          r'(?P<place>.*)\n'
                          r'누적(?P<accumulated>[\d,]*)원')
         res = pat.search(target)
-        return {'result': res, 'pattern': pat, 'group': res.groupdict()}
+        if res:
+            return {'result': res, 'pattern': pat, 'group': res.groupdict()}
+
+        pat = re.compile(r'\[Web발신\]\n'
+                         r'(?P<card_name>.*)매출접수\n'
+                         r'(?P<name_part>.*)님\n'
+                         r'(?P<amount>[\d,]*)원\n'
+                         r'(?P<month>\d*)월(?P<day>\d*)일기준\n'
+                         r'(?P<place>.*)')
+        res = pat.search(target)
+        if res:
+            return {'result': res, 'pattern': pat, 'group': res.groupdict()}
